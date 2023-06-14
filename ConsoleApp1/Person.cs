f56cbfc30e534e0e9482042ecd567477db1289ec
Abhaya <paikraoabhya@gmail.com>
2023-06-14 19:19:21 +0530

    Assingment1

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+       
+            public Person(string First_name, string Last_name, string email, DateTime dob)
+            {
+                this.First_name = First_name;
+                this.Last_name = Last_name;
+                this.Email = email;
+                this.dateOfBirth = dob;
+            }
+            public string First_name { get; set; }
+            public string Last_name { get; set; }
+            public string Email { get; set; }
+            public DateTime dateOfBirth { get; set; }
+
+            // private readonly bool Adult ;
+            // private readonly bool Birthday;
+
+            // private readonly DateTime dateOfBirth;
+
+            public bool Adult
+            {
+                get { return CalculateAdultStatus(); }
+            }
+            public bool IsTodayYourBirthDay
+            {
+                get { return birthDay(); }
+            }
+            private bool birthDay()
+            {
+                DateTime today = DateTime.Today;
+                return (today.Month == dateOfBirth.Month && today.Day == dateOfBirth.Day);
+            }
+            private bool CalculateAdultStatus()
+            {
+                // Calculate adult status based on date of birth
+                DateTime today = DateTime.Today;
+                int age = today.Year - dateOfBirth.Year;
+                if (today < dateOfBirth.AddYears(age))
+                {
+                    age--;
+                }
+                return age >= 18;
+            }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
 Console.WriteLine("Hello, World!");
+var dob = new DateTime(2023, 6, 14);
+Person person = new Person("Abhu", "Paikrao", "paikraoabhya@gmail.com", dob);
+
+Console.WriteLine("Hi" + " " + person.First_name + person.Last_name);
+if (person.Adult)
+{
+    Console.WriteLine("you are Adult");
+}
+else
+{
+    Console.WriteLine("you are not Adult");
+}
+Console.WriteLine(person.Adult);
+if (person.IsTodayYourBirthDay)
+{
+    Console.WriteLine("Today is your birthday ,Happy Birthday " + " " + person.First_name);
+};
+Console.ReadLine();
